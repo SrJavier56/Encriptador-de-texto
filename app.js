0b0a076a72a1618a7b82a28e333cda8b221d92ad
Javier <120879839+SrJavier56@users.noreply.github.com>
2024-08-27 20:48:52 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+function encodeText() {
+    const inputText = document.getElementById('inputText').value.toLowerCase();
+    let encodedText = inputText
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
+    document.getElementById('outputText').value = encodedText;
+}
+
+function decodeText() {
+    const inputText = document.getElementById('inputText').value.toLowerCase();
+    let decodedText = inputText
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
+    document.getElementById('outputText').value = decodedText;
+}
+
+function copyText() {
+    const outputText = document.getElementById('outputText');
+    outputText.select();
+    document.execCommand('copy');
+    alert('Texto copiado al portapapeles');
+}
